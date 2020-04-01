d4d4b535e5149ada5d533a2c73e66deb3d3e0e45
Zakari Badiru <zbadiru@live.co.uk>
2020-04-01 13:25:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def using_concat()
+def using_concat(my_favorite_things, more_favs)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["mario kart", "flatiron school"]
     @all_my_favs = @my_favorite_things.concat(more_favs)
